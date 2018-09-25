cbade74dfdd01e5536d3a01efc97f21f5c83e8b8
kevintudiep <kevintudiep@gmail.com>
2018-09-25 15:28:12 -0500

    Completed Exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
- get 'pictures' => 'pictures#index'
 
+ root  'pictures#index'
+ get   'pictures' => 'pictures#index'
+
+ get   'pictures/:id/edit' => "pictures#edit"
+ patch 'pictures/:id' => "pictures#update"
+
+ post  'pictures' => 'pictures#create'
+ get   'pictures/new' => 'pictures#new'
+
+ delete 'pictures/:id' => 'pictures#destroy'
+
+ get   'pictures/:id' => 'pictures#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
