44501782d81a691c4dbc9a0ec8c28e5e108a1fd8
kevintudiep <kevintudiep@gmail.com>
2018-10-02 08:48:29 -0500

    Adding Validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,27 @@
 class Picture < ApplicationRecord
+
+
+    def self.newest_first
+      Picture.order("created_at DESC")
+    end
+
+    def self.most_recent_five
+      Picture.newest_first.limit(5)
+    end
+
+    def self.created_before(time)
+      Picture.where("created_at < ?", time)
+    end
+
+  validates :artist, :title, :url, presence: true
+  validates :url, uniqueness: true
+  validates_length_of :title, :minimum => 3
+  validates_length_of :title, :maximum => 30
+
+
+
+
+
+
+
 end
