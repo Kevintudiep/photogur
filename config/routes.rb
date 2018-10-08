50d049d86407c5f1f7d38c95a6066ca2c7c8cd47
kevintudiep <kevintudiep@gmail.com>
2018-10-08 16:55:24 -0500

    Added sign up/log in forms, and Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
  resources :pictures
 
  resource :users, only: [:new, :create]
+ resource :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
